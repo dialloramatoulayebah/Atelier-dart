5ed9661fe13bb4e251ef534515f5c52ae78c3ab6
RamaAbdoulaye <dialloramatoulayebah@gmail.com>
2025-10-16 10:40:58 -0400

    Atelier 03 exo 5

---
+++ Atelier03/Types_de_Parametre/exo5.dart
@@ -0,0 +1,7 @@
+void creerCompte({String? utilisateur = "Mark", String? motDePasse = "1234"}) {
+  print("Utilisateur: ${utilisateur!} , Mot de passe: ${motDePasse!}");
+}
+
+void main() {
+  creerCompte();
+}
