0215d47ea30c6cf8f060dc97c36b9787c832dc3f
dialloramatoulayebah <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-10-29 12:55:24 -0400

    Atelier 05 exercice 3

---
+++ Atelier05/exo3.dart
@@ -0,0 +1,13 @@
+void verifierMotdepasse(String mp) {
+  if (mp.length < 6) {
+    throw Exception("Mot de passe trop court !");
+  }
+}
+
+void main() {
+  try {
+    verifierMotdepasse("123");
+  } catch (e) {
+    print("Erreur : $e");
+  }
+}
